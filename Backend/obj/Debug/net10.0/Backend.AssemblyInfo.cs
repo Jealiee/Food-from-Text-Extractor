3c41806a12de1437fa1be79b8f628e2d8239cf74
Jealie <adamus_julia@wp.pl>
2026-02-28 16:56:22 +0100

    Setup frontend

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06641c5511f9d77166c38997b33035535abfbf29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7bdc3eafdcafc58ce587259b28cd2c3c4a44c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
